55245cf58799ddbe7a5000a0dd51a731bcdd82ae
Alren54 <hollyali1234@hotmail.com>
2023-05-07 13:49:01 +0300

    ANAHTAR FEEDBACK VE ALMA ICIN E

--- Assets/Alren/Codes/ItemPickup.cs
+++ Assets/Alren/Codes/ItemPickup.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour
 {
     public Item item;
-
-    private void OnTriggerEnter2D(Collider2D other)
+    private bool isPickable;
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
+        {
+            isPickable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isPickable = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && isPickable)
         {
             InventoryManager.Instance.Add(item);
             print(item.itemName);
